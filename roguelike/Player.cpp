dc8a24c4428972d9f515c4f6a56f509dcb7bc741
danhook98 <s188873@uos.ac.uk>
2025-02-11 15:53:24 +0000

    Update Player.cpp

--- roguelike/Player.cpp
+++ roguelike/Player.cpp
@@ -10,7 +10,7 @@ void Player::DoAttack(int x, int y) {
 	std::cout << damageDelt;
 }
 
-// This should be on the enemy class.
+// TODO: Move this to the enemy class.
 void Player::CheckForPlayer(Enemy& enemy)
 {
 	_isDamaged = enemy.CheckForPlayer(_x, _y);
@@ -22,6 +22,7 @@ void Player::CheckForPlayer(Enemy& enemy)
 }
 
 // Receive an attack from an enemy. 
+// TODO: Replace 'Enemy &enemy' with an integer damage value. An enemy should call this if it can attack the player.
 void Player::ReceiveAttack(Enemy& enemy)
 {
 	// We've just taken damage from an enemy.
